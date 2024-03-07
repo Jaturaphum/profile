d50c04a64d052cbf438e0ade875961eae5578c1c
Jaturaphum <suaprakhon2002@gmail.com>
2024-03-07 19:07:32 +0700

    commit

--- javascript/down.js
+++ javascript/down.js
@@ -1,5 +1,7 @@
-document.getElementById('profile').addEventListener('click', function () {
-  [].map.call(document.querySelectorAll('.profile'), function(el) {
-    el.classList.toggle('profile--open');
-  });
-});
+document.getElementById("profile").addEventListener("click", function () {
+  [].map.call(document.querySelectorAll(".profile"), function (el) {
+    el.classList.toggle("profile--open");
+      }
+    );
+  }
+);
